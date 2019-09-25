b28666116ac4834ba3b2f82057c12759d1df4aaa
mary <goodkarma000>
2019-09-26 00:36:29 +0300

    ml in progress

--- machine_learning/lab2_glvn.py
+++ machine_learning/lab2_glvn.py
@@ -1,48 +1,46 @@
 import cv2
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 
 
+class Picture:
+    def __init__(self, image):
+        self.img = image
 
+    def split_areas(self, num_x, num_y):
+        imgwidth, imgheight = self.img.size
+        step_x = round(imgwidth/num_x)
+        step_y = round(imgheight/num_y)
+        i = 0
+        while i <= imgwidth:
+            j = 0
+            while j <= imgheight:
+                box = (i, j, i + step_x, j + step_y)
+                a = self.img.crop(box)
+                a.save(f'{i}{j}.png')
+                print(a)
+                j += step_y
+            i += step_x
 
-#  OpenCV port of 'TENG' algorithm (Krotkov86)
-# double tenengrad(const cv::Mat& src, int ksize)
-# {
-#     cv::Mat Gx, Gy;
-#     cv::Sobel(src, Gx, CV_64F, 1, 0, ksize);
-#     cv::Sobel(src, Gy, CV_64F, 0, 1, ksize);
-#
-#     cv::Mat FM = Gx.mul(Gx) + Gy.mul(Gy);
-#
-#     double focusMeasure = cv::mean(FM).val[0];
-#     return focusMeasure;
-# }
-#
-# // OpenCV port of 'GLVN' algorithm (Santos97)
-# double normalizedGraylevelVariance(const cv::Mat& src)
-# {
-#     cv::Scalar mu, sigma;
-#     cv::meanStdDev(src, mu, sigma);
-#
-#     double focusMeasure = (sigma.val[0]*sigma.val[0]) / mu.val[0];
-#     return focusMeasure;
+                # try:
+                #     o = self.img.crop(area)
+                #     o.save(os.path.join(path, "PNG", "%s" % page, "IMG-%s.png" % k))
+                # except:
+                #     pass
+                # k += 1
 
-def glvn(img): # GLVN - отношение глобальной дисперсии к глобальному среднему яркости
-    mu, sigma = cv2.meanStdDev(img)
-    # print(f'mu:{mu}, sigma: {sigma}')
-    focus_measure = sigma[0][0]**2/mu[0][0]
-
-    return focus_measure
 
 
 
 def main():
     filename = 'pic.png'
-    img = cv2.imread(filename)
-    # print(img)
-    # src = np.array(img)
-    # grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype('f4')
-    print(glvn(img))
-    # print(glvn(grayImg))
+    # img = cv2.imread(filename)
+    img = Image.open(filename)
+    pict = Picture(img)
+    pict.split_areas(2,2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
