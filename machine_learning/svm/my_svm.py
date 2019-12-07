8d681f5e10330660fd6f6c40bca14552b2748c24
mary <maniya.chernova@gmail.com>
2019-12-08 00:39:49 +0300

    added lab3 svm

--- machine_learning/svm/my_svm.py
+++ machine_learning/svm/my_svm.py
@@ -1,91 +1,55 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 from sklearn import svm
+from cvxopt import matrix, solvers
+
 
 def func(x):
-    return x**2 + 2*x - 6
-    # return math.sin(x) + 10
+    return (x - 3) ** 2 + 2 * x - 6
+#     return math.sin(x) + 10
+
 
 def create_data(num: int):
     X = []
     y = []
     for i in range(num):
-        value_x = random.randint(0,20)
-        max_y = round(func(10))
-        max_y = round(func(10))
-        value_y = random.randint(0,20)
+        value_x = float(random.randint(0, 20))
+        value_y = float(random.randint(0, 20))
         coord = [value_x, value_y]
         X.append(coord)
         if func(value_x) > value_y:
-            y.append(1)
+            y.append(1.0)
         else:
-            y.append(-1)
+            y.append(-1.0)
 
     return np.array(X), np.array(y)
 
 
-def scalar_prod(w1,w2):
-    s = 0
-    for i in range(0,len(w1)):
-        s+= w1[i]*w2[i]
-    return s
+def find_lambda(X, y):
+    l = X.shape[0]
+    K = np.multiply(X.T, y)
+    P = matrix(1 / 2 * np.dot(K.T, K))
+    q = matrix(-np.ones((l, 1)))  # solver решает задачу минимизации, а мы максимизируем функцию
+    G = matrix(-np.eye(l))
+    h = matrix(np.zeros(l))
+    A = matrix(y.reshape(1, -1))
+    b = matrix(0.0)
+    sol = solvers.qp(P, q, G, h, A, b)
+    lambdas = np.array(sol['x'])
+    return lambdas
 
 
-def r_exp(x_i1, x_i2, y_i,w1,w2):
-    return 1 - 1/(1+math.exp(-y_i*(w1*x_i1+w2*x_i2)))
-
-def a_sigma(x, w):
-    return 1/(1+math.exp(-w[0]*x[0]-w[1]*x[1]))
-
-
-def sum1_l(x, y, w1, w2):
-    s=0
-    x1 = x[:,0]
-    x2 = x[:,1]
-    l = len(y)
-    for i in range(0,l):
-        s+=y[i]*x1[i]*r_exp(x1[i],x2[i],y[i],w1,w2)
-    return s/l
-
-def sum2_l(x, y, w1, w2):
-    s=0
-    x1 = x[:,0]
-    x2 = x[:,1]
-    l = len(y)
-    for i in range(0,l):
-        s+=y[i]*x2[i]*r_exp(x1[i],x2[i],y[i],w1,w2)
-    return s/l
-
-
-def normal_grad(X,y, w_0:list, k, C):
-    w1 = w_0[0]
-    w2 = w_0[1]
-    w1 = w1 + k*sum1_l(X,y,w1,w2)-k*C*w1
-    w2 = w2 + k*sum2_l(X,y,w1,w2)-k*C*w2
-    w = [w1,w2]
-    wx = [w[i]-w_0[i] for i in range(0,len(w))]
-    eps = math.sqrt(scalar_prod(wx,wx))
-    return w, eps
-
-
-def LogRegression(X,y,k,C):
-    w_0 = [0,0]#initial weights
-    e = 1e-5
-    N = 1e+3#iterations
-    i = 0
-    eps = 10
-    while (eps > e) and (i < N):
-        w, eps = normal_grad(X,y,w_0,k,C)
-        w_0 = w
-        i+=1
-    # a_x = [a_sigma(x,w) for x in X]
-    a_x = [np.sign(scalar_prod(x,w)) for x in X]
-    return a_x
-
-
-
+def sep_by_function(X, sep):
+    data_len = X.shape[0]
+    X_1 = []
+    X_2 = []
+    for i in range(data_len):
+        if X[i][1] < sep(X[i][0]):
+            X_1.append(X[i])
+        else:
+            X_2.append(X[i])
+    return np.array(X_1), np.array(X_2)
 
 
 def data_separation(X, y):
@@ -101,22 +65,14 @@ def data_separation(X, y):
     return np.array(X_1), np.array(X_2)
 
 
-
-def data_plot(X, y, ax, data_len):
-    x = np.linspace(0, 20, data_len)
-    f = [func(el) for el in x]
-
-    plt.ylim(
-        f[0], 30
-    )
-
-
+def data_plot(X, y, ax, coeff):
+    x = np.linspace(0, 21)
     X_true_1, X_true_2 = data_separation(X, y)
     plt.subplot(311)
     plt.title('Real')
-    plt.scatter(X_true_1[:, 0], X_true_1[:, 1], c='red',  marker=">")
-    plt.scatter(X_true_2[:, 0], X_true_2[:, 1], c='green',  marker=(5, 2))
-    plt.plot(x, f)
+    plt.scatter(X_true_1[:, 0], X_true_1[:, 1], c='red', marker=">")
+    plt.scatter(X_true_2[:, 0], X_true_2[:, 1], c='green', marker=(5, 2))
+    plt.plot(x, func(x))
     plt.ylim(
         -10, 30
     )
@@ -124,19 +80,27 @@ def data_plot(X, y, ax, data_len):
     X_pred_1, X_pred_2 = data_separation(X, ax)
     plt.subplot(312)
     plt.title('Predicted by sklearn')
-    plt.scatter(X_pred_1[:, 0], X_pred_1[:, 1], c='red',  marker=">")
-    plt.scatter(X_pred_2[:, 0], X_pred_2[:, 1], c='green',  marker=(5, 2))
-    plt.plot(x, f)
+    plt.scatter(X_pred_1[:, 0], X_pred_1[:, 1], c='red', marker=">")
+    plt.scatter(X_pred_2[:, 0], X_pred_2[:, 1], c='green', marker=(5, 2))
+    #     plt.plot(x, func(x))
     plt.ylim(
         -10, 30
     )
 
-    # X_pred_1, X_pred_2 = data_separation(X, ax)
+    w = np.sum(coeff * y.reshape(-1, 1) * X, axis=0)
+    cond = (coeff > 1e-4).reshape(-1)
+    b = y[cond] - np.dot(X[cond], w)
+    b1 = y - np.dot(X, w)
+    bias = b[0]
+    k = -w[0] / w[1]
+    b = -bias / w[1]
+    sep_plane = lambda x: x * k + b
     plt.subplot(313)
     plt.title('Predicted by my_svm')
-    # plt.scatter(X_pred_1[:, 0], X_pred_1[:, 1], c='red', marker=">")
-    # plt.scatter(X_pred_2[:, 0], X_pred_2[:, 1], c='green', marker=(5, 2))
-    plt.plot(x, f)
+    plt.plot(x, sep_plane(x))
+    X_pred_1, X_pred_2 = sep_by_function(X, sep_plane)
+    plt.scatter(X_pred_1[:, 0], X_pred_1[:, 1], c='red', marker=">")
+    plt.scatter(X_pred_2[:, 0], X_pred_2[:, 1], c='green', marker=(5, 2))
     plt.ylim(
         -10, 30
     )
@@ -145,17 +109,14 @@ def data_plot(X, y, ax, data_len):
 
 
 def main():
-    data_len = 36
+    data_len = 100
     X, y = create_data(data_len)
-    print(X, y)
-    clf = svm.SVC(C=100, gamma='scale')
-    clf.fit(X,y)
+    clf = svm.SVC()
+    clf.fit(X, y)
     a_x = clf.predict(X)
-    print(a_x)
-    data_plot(X, y, a_x, data_len)
+    lambdas = find_lambda(X, y)
+    data_plot(X, y, a_x, lambdas)
     plt.show()
-
-
 
 
 if __name__ == '__main__':
